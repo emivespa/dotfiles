1e59965cdea1b29ead144745481f930a0d7bca8c
Emilio Vesprini <emivespa1@gmail.com>
2023-08-06 21:33:21 +0000

    replace gitgutter with gitsigns

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,13 +1,25 @@
 return {
 	{
-		"https://github.com/airblade/vim-gitgutter",
+		"https://github.com/lewis6991/gitsigns.nvim",
 		config = function()
-			vim.cmd([[
-				hi clear GitGutterAddLine          | hi link GitGutterAddLine            diffAdded
-				hi clear GitGutterChangeLine       | hi link GitGutterChangeLine         diffLine
-				hi clear GitGutterDeleteLine       | hi link GitGutterDeleteLine         diffRemoved
-				hi clear GitGutterChangeDeleteLine | hi link GitGutterChangeDeleteLine   diffLine
-			]])
+			require('gitsigns').setup({
+				signs = {
+					add          = { text = '+' },     -- add          = { text = '│' },
+					change       = { text = '~' },     -- change       = { text = '│' },
+					delete       = { text = '_' },     -- delete       = { text = '_' },
+					topdelete    = { text = '‾' },     -- topdelete    = { text = '‾' },
+					changedelete = { text = '~' },     -- changedelete = { text = '~' },
+					untracked    = { text = '?' },     -- untracked    = { text = '┆' },
+				},
+				-- word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+				current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+				current_line_blame_opts = {
+					virt_text = true,
+					virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
+					delay = 1000,
+				},
+				current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+			})
 		end,
 	},
 }
