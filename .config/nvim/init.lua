0aa6a76c84d83552af42173e40e1fd4e1945200c
Emilio Vesprini <emivespa1@gmail.com>
2023-08-07 00:04:46 +0000

    restore matchit and matchparen default plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,8 +25,8 @@ else
 			rtp = {
 				disabled_plugins = {
 					"gzip",
-					"matchit",
-					"matchparen",
+					-- "matchit",
+					-- "matchparen",
 					-- "netrwPlugin",
 					"tarPlugin",
 					"tohtml",
